197b530b28ee52eb5f11e7718f70c4ff606d2171
asgx1 <gisaaimesavio@gmail.com>
2026-05-08 10:17:02 +0200

    Full function of isBalanced

--- Balanced.java
+++ Balanced.java
@@ -7,10 +7,32 @@ public class Balanced{
         
     }
     public static boolean isBalanced(String s){
-        //
-        
-        return false;
-}
-        
-    
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            }
+
+            else if (ch == ')' || ch == '}' || ch == ']') {
+
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+
+    }    
 }
